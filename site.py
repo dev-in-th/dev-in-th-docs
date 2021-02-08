1b90228e789f9894eb984b7ea1a6cc04ce7d798b
Max Pijittum <mpijittum@fb.com>
2021-02-08 17:17:39 +0800

    Add DNS to diagram

--- site.py
+++ site.py
@@ -3,15 +3,19 @@ from diagrams.onprem.database import Postgresql
 from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.network import Nginx
 from diagrams.onprem.network import Gunicorn
+from diagrams.onprem.dns import Coredns
+from diagrams.gcp.network import DNS
 
 with Diagram("site", show=False):
+    dns = DNS("Cloudflare DNS")
     with Cluster("GCP Instances"):
         db = Postgresql("PostgreSQL")
         cache = Redis("Redis Cache")
         with Cluster("Discourse"):
             nginx = Nginx("Webserver")
             gunicorn = Gunicorn("Discourse")
-        
+    
+    dns >> nginx
     nginx >> gunicorn
     gunicorn >> cache
     gunicorn >> db
